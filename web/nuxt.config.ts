d799900138faf751dfa9769a59782b12be1782b5
Kum Brian Ihimbru <kumbrianihimbru@gmail.com>
2026-06-16 22:01:42 +0100

    chore: configure favicon in nuxt.config.ts

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -27,5 +27,12 @@ export default defineNuxtConfig({
     smtpPort: process.env.SMTP_PORT || '587',
     smtpUser: process.env.SMTP_USER || '',
     smtpPass: process.env.SMTP_PASS || ''
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
   }
 })
