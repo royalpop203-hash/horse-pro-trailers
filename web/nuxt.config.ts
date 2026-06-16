34f5150ed4f9d743f2ff95b14111e159cfae0d9e
Kum Brian Ihimbru <kumbrianihimbru@gmail.com>
2026-06-16 22:04:50 +0100

    chore: use site logo as favicon

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/png', href: '/images/logo.png' }
       ]
     }
   }
