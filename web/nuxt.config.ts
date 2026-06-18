b596161e99d48557c7f314e09f3c787c9db99e25
Kum Brian Ihimbru <kumbrianihimbru@gmail.com>
2026-06-18 08:07:02 +0100

    feat: add Google Tag Manager integration to nuxt configuration

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -32,6 +32,22 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: 'icon', type: 'image/png', href: '/images/logo.png' }
+      ],
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TVXW334T');`,
+          tagPosition: 'bodyOpen'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVXW334T" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          tagPosition: 'bodyOpen'
+        }
       ]
     }
   }
